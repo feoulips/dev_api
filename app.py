58ecf281090114c6dc1020b6c819628e6850e469
feoulips <felipesilvasoares32@gmail.com>
2021-09-27 10:13:23 -0300

    Adicionado app.py onde tem o exemplo de REST API para manipular dados referente a um desenvolvedor e suas habilidades.

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, jsonify, request
+import json
+
+app = Flask (__name__)
+
+desenvolvedores = [
+    {'nome':'Felipe',
+     'habilidades':['Python', 'Flask']},
+    {'nome': 'Soares',
+     'habilidades':['Python', 'Django']}
+]
+
+# devolve um desenvolvedor pelo, ID também altera e deleta no desenvolvedor
+@app.route('/dev/<int:id>/', methods=['GET', 'PUT', 'DELETE'])
+def desenvolvedor(id):
+    if request.method == 'GET':
+        try:
+            response = desenvolvedores[id]
+        except IndexError:
+            mensagem = 'Desenvolvedor de ID {} não existe'.format(id)
+            response = {'status':'erro', 'mensagem':mensagem}
+        except Exception:
+            mensagem = 'Erro desconhecido. Procure o administrador da Api'
+            response = {'status':'erro', 'mensagem':mensagem}
+        return jsonify(response)
+    elif request.method == 'PUT':
+        dados = json.loads(request.data)
+        desenvolvedores[id] = dados
+        return jsonify(dados)
+    elif request.method == 'DELETE':
+        desenvolvedores.pop(id)
+        return jsonify({'status':'sucesso', 'mensagem':'Registro excluido'})
+
+# lista todos os desenvolvedor e permite registrar um novo desenvolvedor
+@app.route('/dev/', methods=['POST', 'GET'])
+def lista_desenvolvedores():
+    if request.method =='POST':
+        dados = json.loads(request.data)
+        desenvolvedores.append(dados)
+        return jsonify({'status':'sucesso', 'mensagem':'Registro inserido'})
+    elif request.method == 'GET':
+        return jsonify(desenvolvedores)
+
+if __name__ == '__main__':
+    app.run(debug=True)
